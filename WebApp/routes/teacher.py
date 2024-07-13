32889ff5037aafe5c5a7a7c5750975e61ae4e9dc
0932773-Simone <90318458+SimoneCalf@users.noreply.github.com>
2024-07-13 15:26:40 +0200

    templates

--- WebApp/routes/teacher.py
+++ WebApp/routes/teacher.py
@@ -4,5 +4,17 @@ app = Flask(__name__)
 teacher_bp = Blueprint('teacher', __name__)
 
 @teacher_bp.route('/')
-def student_home():
+def teacher_home():
     return render_template('teacher.html')
+
+@teacher_bp.route('/admin_teachers')
+def admin_teachers():
+    return render_template('admin_teachers.html')
+
+@teacher_bp.route('/manage_students')
+def manage_students():
+    return render_template('manage_students.html')
+
+@teacher_bp.route('/manage_teachers')
+def manage_teachers():
+    return render_template('manage_teachers.html')
---
+++ WebApp/static/admin_teachers.js
@@ -0,0 +1,7 @@
+function navigateToManageStudens() {
+    window.location.href = '/teacher/manage_students';
+}
+
+function navigateToManageTeachers() {
+    window.location.href = '/teacher/manage_teachers';
+}
