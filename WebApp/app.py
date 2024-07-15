e391f205c18d09e8ff8bc17409d8badffcd68f21
0932773-Simone <90318458+SimoneCalf@users.noreply.github.com>
2024-07-15 17:41:51 +0200

    start db

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 from routes.student import student_bp
 from routes.teacher import teacher_bp
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.register_blueprint(student_bp, url_prefix='/student')
 app.register_blueprint(teacher_bp, url_prefix='/teacher')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/our_users'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# ceate model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
 @app.route('/')
 def home():
