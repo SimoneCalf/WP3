775161da63105a6b611edab0d765b940855b599c
0932773-Simone <90318458+SimoneCalf@users.noreply.github.com>
2024-07-16 12:22:05 +0200

    bezig met db

--- WebApp/app.py
+++ WebApp/app.py
@@ -3,27 +3,19 @@ from routes.student import student_bp
 from routes.teacher import teacher_bp
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models.sql import *
 
 app = Flask(__name__)
 app.register_blueprint(student_bp, url_prefix='/student')
 app.register_blueprint(teacher_bp, url_prefix='/teacher')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/our_users'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+sqlalchemy = SQLAlchemy(app)
 
-db = SQLAlchemy(app)
-
-# ceate model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    age = db.Column(db.Integer)
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
 
 @app.route('/')
 def home():
+    get_teacher_info()
     return render_template('home.html')
 
 
---
+++ WebApp/models/sql.py
@@ -0,0 +1,13 @@
+from flask_mysqldb import MySQL
+
+
+mysql = MySQL()
+
+# query to get all the teacher information
+def get_teacher_info():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM teacher')
+    result = cursor.fetchall()
+    print(result)
+    cursor.close()
+    return result
